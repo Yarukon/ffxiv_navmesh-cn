f48ed06081bf82a27175c842abfb8d02f04370b9
AtmoOmen <atmoomen2004@outlook.com>
2025-05-16 21:35:28 +0800

    避免单次占用过多

--- vnavmesh/NavmeshManager.cs
+++ vnavmesh/NavmeshManager.cs
@@ -266,7 +266,9 @@ public sealed class NavmeshManager : IDisposable
         var parallelOptions = new ParallelOptions
         {
             CancellationToken      = cancel,
-            MaxDegreeOfParallelism = Math.Max(1, Environment.ProcessorCount - 1)
+            MaxDegreeOfParallelism = Environment.ProcessorCount <= 8
+                                         ? Math.Max(1, Environment.ProcessorCount / 3)
+                                         : Math.Max(1, Environment.ProcessorCount / 2)
         };
 
         var tileTasks = new List<(int X, int Z)>();
